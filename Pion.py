75f647fe54313edaeceb87ceac81c2a18d02a645
MaxBlouin <max_blu8@hotmail.com>
2013-11-17 21:18:37 -0500

    update pion

--- Pion.py
+++ Pion.py
@@ -6,6 +6,7 @@ Created on 2013-11-08
 
 @author: maximeblouin
 '''
+from chess import plateau
 
 class Pion:
     '''Le pion se déplace vers l'avant d'une seule case à la fois, à l'exception du premier coup où il peut se deplacer de deux cases vers l'avant. Ce pion mange les autres pièces en diagonale directe vers l'avant.'''
@@ -24,22 +25,20 @@ class Pion:
 
     def deplacementValide(self,pos_dep,pos_arr,plateau):
         '''La fonction permet de d'afficher les déplacements possibles d'un pion'''
-        self.lp = [] #Car on a besoin d'une liste de coup possible
         pos_dep=self.pos
         pieceArr=plateau.getpiece(pos_arr[0],pos_arr[1])
         if pieceArr!=None and pieceArr.color==self.color:
             return False
         elif pos_dep and pos_arr in plateau:
-            
             #Pour interdire les déplacements de cote et vers l'arriere
             if 'blanc' in self.colour and pos_arr[0] < pos_dep[0]:
                 return False
             elif 'noir' in self.colour and pos_arr[0] > pos_dep[0]:
                 return False
-            elif pos_dep[0] == pos_arr[0]:
+            if pos_dep[0] == pos_arr[0]:
                 return False
             #Pour les déplacements en diagonale immédiate pour manger d'autres pieces
-            elif pos_arr in plateau:
+            if pos_arr in plateau:
                 if abs(pos_arr[1]-pos_dep[1]) == abs(pos_arr[0]-pos_dep[0]) == 1:
                     return True
             #Pour tout autre coup, on ajoute la case d'après(+1 en y)
@@ -49,16 +48,14 @@ class Pion:
                     if abs(pos_arr[0]-pos_dep[0]) == 1:
                         return True
                     #Regle du premier deplacement, mouvement de deux cases permis
-                    elif 'noir' in self.color and pos[1]==6:
-                        if abs(pos_arr[0]-pos_dep[0]) == 2: #On permet l'ajout de 1 ou 2 à la valeur verticale (y)
-                            return True
-                    elif 'blanc' in self.color and pos[1]==1:
-                        if abs(pos_arr[0]-pos_dep[0]) == 2:
-                            return True
+                    elif 'noir' in self.color and pos_dep[1]==6 and abs(pos_arr[0]-pos_dep[0]) == 2:
+                    #On permet l'ajout de 1 ou 2 à la valeur verticale (y)
+                        return True
+                    elif 'blanc' in self.color and pos_dep[1]==1 and abs(pos_arr[0]-pos_dep[0]) == 2 :
+                        return True
             #Le pion ne peut pas passer par dessus les autres pieces
             for i in range (pos_dep[0],pos_arr[0]):
                 if plateau.getPiece(pos_dep[0],i) != None:
-                plateau.getPiece(pos_dep[0],i).color == self.color:
                     return False
     
     def promotionpion(self, plateau, target):
@@ -67,4 +64,8 @@ class Pion:
             prom = \
             raw_input("Vous pouvez promouvoir votre pion en:\n[Ca]valier [Re]ine [T]our [F]ou  : ")
         plateau[target].promouvoir(prom)
+        
+    def dame(self, pos_dep, pos_arr):
+        if self.tour(pos_dep, pos_arr) or self.fou(pos_dep, pos_arr):
+            return True
         
